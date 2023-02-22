a3377d41254e7f3fa78e40f0b20307d4cd3e1cca
Gigacee <gigacee@gigacreation.jp>
2023-02-22 15:42:32 +0900

    Add project

---
+++ Assets/Sample/Scripts/Csv2CollectionsSample.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace GigaCreation.Tools.Csv2Collections.Sample
+{
+    public class Csv2CollectionsSample : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+    }
+}
